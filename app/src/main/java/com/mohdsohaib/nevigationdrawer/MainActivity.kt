1a215701132cbf53149fcb65031b30c96df28527
Sohaib <mohdsohaib78692@gmail.com>
2023-03-26 16:34:49 +0530

    Added Bottom Navigation

--- app/src/main/java/com/mohdsohaib/nevigationdrawer/MainActivity.kt
+++ app/src/main/java/com/mohdsohaib/nevigationdrawer/MainActivity.kt
@@ -5,16 +5,50 @@ import android.os.Bundle
 import android.view.MenuItem
 import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.mohdsohaib.nevigationdrawer.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityMainBinding
     lateinit var toggle: ActionBarDrawerToggle
+    lateinit var bottom_toggle: BottomNavigationView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.apply {
+           bottom_toggle = BottomNavigationView(this@MainActivity)
+           navBottom.selectedItemId = R.id.bottom_home
+
+           navBottom.setOnItemSelectedListener {
+               when(it.itemId){
+                   R.id.bottom_post -> {
+                       Toast.makeText(this@MainActivity,"Post Clicked",Toast.LENGTH_SHORT).show()
+                   }
+
+                   R.id.bottom_search -> {
+                       Toast.makeText(this@MainActivity,"Search Clicked",Toast.LENGTH_SHORT).show()
+                   }
+
+                   R.id.bottom_home -> {
+                       Toast.makeText(this@MainActivity,"Home Clicked",Toast.LENGTH_SHORT).show()
+                   }
+
+                   R.id.bottom_favorite -> {
+                       Toast.makeText(this@MainActivity,"Favorite Clicked",Toast.LENGTH_SHORT).show()
+                   }
+
+                   R.id.bottom_profile -> {
+                       Toast.makeText(this@MainActivity,"Profile Clicked",Toast.LENGTH_SHORT).show()
+                   }
+               }
+               true
+           }
+
+        }
+
 
         binding.apply {
             toggle = ActionBarDrawerToggle(this@MainActivity, drawerLayout, 0, 0)
